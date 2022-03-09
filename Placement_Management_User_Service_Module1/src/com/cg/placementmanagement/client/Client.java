2254668e83afee3e6e333d76924f05208a564e00
sharathbn <sharathbn514@gamil.com>
2022-03-10 00:12:12 +0530

    first commit

--- Placement_Management_User_Service_Module1/src/com/cg/placementmanagement/client/Client.java
+++ Placement_Management_User_Service_Module1/src/com/cg/placementmanagement/client/Client.java
@@ -9,11 +9,15 @@ public class Client {
 	public static void main(String[] args) {
 	 UserService service = new UserServiceImpl();
 	 User user=new User();
-	user.setId(20);
-	user.setName("sharath");
+	user.setId(23);
+	user.setName("sharathghghhg");
 	user.setType("shj");
 	user.setPassword("sjsjkk");
-	service.addNewUser(user);
+	service.updateUser(user);
+	
+	
+
+	
 	}
 
 }
--- Placement_Management_User_Service_Module1/src/com/cg/placementmanagement/entities/User.java
+++ Placement_Management_User_Service_Module1/src/com/cg/placementmanagement/entities/User.java
@@ -1,12 +1,16 @@
 package com.cg.placementmanagement.entities;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="user")
-public class User {
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	private int id;
 	private String name;
